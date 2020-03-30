27a2ca5dbe3ab76fdbbe567778ad92d234f38b70
Juan Gonzalo Quiroz Cadavid <jquiro12@eafit.edu.co>
2020-03-30 11:16:44 -0500

    Use of lombok accross the project

--- user-service/src/main/java/com/ecommercepractice/userservice/controller/UserControlle.java
+++ user-service/src/main/java/com/ecommercepractice/userservice/controller/UserControlle.java
@@ -2,6 +2,7 @@ package com.ecommercepractice.userservice.controller;
 
 import com.ecommercepractice.userservice.models.User;
 import com.ecommercepractice.userservice.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,6 +10,7 @@ import java.util.List;
 
 @RequestMapping("api/v1/user")
 @RestController
+@Slf4j // Lombok
 public class UserControlle {
     private final UserService userService;
 
@@ -19,29 +21,34 @@ public class UserControlle {
 
     @PostMapping
     public void addUser(@RequestBody User user){
+        log.info("USER | POST  { " +user.toString() + "}");
         userService.addUser(user);
     }
 
     @GetMapping
     public List<User> getAllUser(){
+        log.info("USER | GET ALL");
         return userService.getAllusers();
     }
 
     //api/v1/user/:user_name
-    @GetMapping(path = "{username}")
-    public User getUserByUserName(@PathVariable("username") String user_name){
-        return userService.getUserbyUserName(user_name)
+    @GetMapping(path = "{user_id}")
+    public User getUserByUserName(@PathVariable("user_id") String user_id){
+        log.info("USER | GET | USER_ID  { " + user_id + "}");
+        return userService.getUserbyUserName(user_id)
                 .orElse(null);
     }
-    @DeleteMapping(path = "{username}")
-    public void deleteUser(@PathVariable("username") String user_name){
-        userService.deleteUser(user_name);
+    @DeleteMapping(path = "{user_id}")
+    public void deleteUser(@PathVariable("user_id") String user_id){
+        log.info("USER | DELETE | USER_ID  { " + user_id + "}");
+        userService.deleteUser(user_id);
     }
 
 
-    @PutMapping(path = "{username}")
-    public void updateUser(@PathVariable("username") String user_name, @RequestBody User user){
-        userService.updateUser(user_name,user);
+    @PutMapping(path = "{user_id}")
+    public void updateUser(@PathVariable("user_id") String user_id, @RequestBody User user){
+        log.info("USER | PUT | USER_ID  { " + user_id +  "  - USER {" + user.toString() + "}" + "}");
+        userService.updateUser(user_id,user);
 
     }
 
