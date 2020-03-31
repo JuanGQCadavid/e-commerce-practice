a87f259db84cd63194959211e5de8cb0ebb55bdd
Juan Gonzalo Quiroz Cadavid <jquiro12@eafit.edu.co>
2020-03-31 10:12:58 -0500

    User Data validation not null and max value

--- user-service/src/main/java/com/ecommercepractice/userservice/models/User.java
+++ user-service/src/main/java/com/ecommercepractice/userservice/models/User.java
@@ -6,16 +6,32 @@ import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import lombok.ToString;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
 public class User {
+    @NotNull
+    @Size(min = 6, max=24)
     private  String userId;
+
+    @NotNull
+    @Size(max = 24)
     private  String firstName;
+
+    @NotNull
+    @Size(max = 24)
     private  String secondName;
+
+    @NotNull
     private  boolean isActive;
+
+    @NotNull
     private  String email;
 
 }
