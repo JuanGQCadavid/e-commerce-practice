fa0211bca738201f8081098c887d39d8f268af7f
Juan Gonzalo Quiroz Cadavid <jquiro12@eafit.edu.co>
2020-04-07 15:41:56 -0500

    Swagger implemented

--- user-service/src/main/java/com/ecommercepractice/userservice/models/User.java
+++ user-service/src/main/java/com/ecommercepractice/userservice/models/User.java
@@ -1,5 +1,7 @@
 package com.ecommercepractice.userservice.models;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
@@ -15,27 +17,34 @@ import javax.validation.constraints.Size;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@ApiModel(value="User",description="Module used to represent the user data onto the system")
 public class User {
     @NotNull
     @Size(min = 6, max=24)
+    @ApiModelProperty(value = "Unique user identifier")
     private  String userId;
 
     @NotNull
     @Size(min= 6, max = 24)
+    @ApiModelProperty(value = "User's password")
     private String password;
 
     @NotNull
     @Size(max = 24)
+    @ApiModelProperty(value = "User's firstName")
     private  String firstName;
 
     @NotNull
     @Size(max = 24)
+    @ApiModelProperty(value = "User's secondName")
     private  String secondName;
 
     @NotNull
+    @ApiModelProperty(value = "Flag that indicates the user state, True indicates tha the user is Activate, no activate in the other case ")
     private  boolean isActive;
 
     @NotEmpty
+    @ApiModelProperty(value = "User's email")
     private  String email;
 
 }
