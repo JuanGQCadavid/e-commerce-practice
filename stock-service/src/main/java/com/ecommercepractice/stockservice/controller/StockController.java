57978e55e05d30757c5f215902f83b4b35594402
Juan Gonzalo Quiroz Cadavid <jquiro12@eafit.edu.co>
2020-05-10 16:16:04 -0500

    application propierties

--- stock-service/src/main/java/com/ecommercepractice/stockservice/controller/StockController.java
+++ stock-service/src/main/java/com/ecommercepractice/stockservice/controller/StockController.java
@@ -18,9 +18,10 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import javax.validation.Valid;
+
 import javax.ws.rs.BeanParam;
+import javax.ws.rs.core.Response;
 import java.util.List;
 import java.util.stream.Collectors;
 
