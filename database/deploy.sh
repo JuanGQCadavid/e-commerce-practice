d71f25e704f8740d2237783b4c8a08ad5afdb6ec
Juan Gonzalo Quiroz Cadavid <jquiro12@eafit.edu.co>
2020-05-15 10:48:06 -0500

    Test4

--- database/deploy.sh
+++ database/deploy.sh
@@ -10,10 +10,19 @@ else
 fi
 
 
-if ! [ -x "$(docker volume inspect mysql-db-data)" ]; then
+if ! [ -z "$(docker volume inspect mysql-db-data)" ]; then
     echo "Creating volumne"
     docker volume create mysql-db-data
+else
+    echo "Database volume already created"
 fi
 
-docker rm -f mysql-db
+if  [ -z "$(docker inspect mysql-db)" ]; then
+    echo "Removing old mysl-db"
+    docker rm -f mysql-db
+fi
+
+
 docker run -d -p 3306:3306 --name mysql-db -e MYSQL_ROOT_PASSWORD=secret -v mysql-db-data:/var/lib/mysql mysql
+
+
