8e5a16f935227ae5dcf3f3d9a422549840710240
Juan Gonzalo Quiroz Cadavid <jquiro12@eafit.edu.co>
2020-05-20 14:38:41 -0500

    Starting project

---
+++ assembler-service/src/main/java/com/ecommercepractice/assemblerservice/AssemblerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommercepractice.assemblerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssemblerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssemblerServiceApplication.class, args);
+	}
+
+}
