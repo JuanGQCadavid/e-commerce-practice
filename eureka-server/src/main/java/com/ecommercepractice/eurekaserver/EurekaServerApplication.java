c46f96175c18c3a65f1e72dd3f47d6feb74af47f
Juan Gonzalo Quiroz Cadavid <Juan.G.Quiroz.-ND@disney.com>
2020-07-03 12:04:01 -0500

    Eureka Check point

--- eureka-server/src/main/java/com/ecommercepractice/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/ecommercepractice/eurekaserver/EurekaServerApplication.java
@@ -1,8 +1,16 @@
 package com.ecommercepractice.eurekaserver;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -13,3 +21,16 @@ public class EurekaServerApplication {
 	}
 
 }
+@RestController
+class ServiceInstanceRestController {
+
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
+	@RequestMapping("/service-instances/{applicationName}")
+	public List<ServiceInstance> serviceInstancesByApplicationName(
+			@PathVariable String applicationName) {
+		return this.discoveryClient.getInstances(applicationName);
+	}
+}
+
--- stock-service/src/main/java/com/ecommercepractice/stockservice/StockServiceApplication.java
+++ stock-service/src/main/java/com/ecommercepractice/stockservice/StockServiceApplication.java
@@ -2,7 +2,9 @@ package com.ecommercepractice.stockservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class StockServiceApplication {
 	public static void main(String[] args) {
